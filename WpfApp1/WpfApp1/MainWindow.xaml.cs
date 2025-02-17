a93d21b6fc6423fee7235eaedea33dee66c59c44
Vojtěch Jiruš <jirusv23@spstrutnov.cz>
2025-02-17 13:20:12 +0100

    hokaido

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -16,14 +16,38 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private int cislo;
+        private string displayValue;
+
         public MainWindow()
         {
             InitializeComponent();
+            cislo = 0;
+            displayValue = "0";
+
+            Cislo_0.Click += (sender, e) => AppendNumber(0, "0");
+            Cislo_1.Click += (sender, e) => AppendNumber(1, "1");
+            Cislo_2.Click += (sender, e) => AppendNumber(2, "2");
+            Cislo_3.Click += (sender, e) => AppendNumber(3, "3");
+            Cislo_4.Click += (sender, e) => AppendNumber(4, "4");
+            Cislo_5.Click += (sender, e) => AppendNumber(5, "5");
+            Cislo_6.Click += (sender, e) => AppendNumber(6, "6");
+            Cislo_7.Click += (sender, e) => AppendNumber(7, "7");
+            Cislo_8.Click += (sender, e) => AppendNumber(8, "8");
+            Cislo_9.Click += (sender, e) => AppendNumber(9, "9");
+
+
         }
-
-        private void Tlacitko_Click(object sender, RoutedEventArgs e)
+        private void AppendNumber(int number, string numberString)
         {
-
+            displayValue += numberString;
+            number.
+        }
+        private void NegateValue()
+        {
+            currentValue *= -1;
+            Label.Content = currentValue.ToString();
         }
     }
 }
