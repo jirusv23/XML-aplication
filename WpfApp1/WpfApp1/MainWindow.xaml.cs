4f674c7d7ad12970d16c5a836f8d288f76de4698
Vojtěch Jiruš <jirusv23@spstrutnov.cz>
2025-02-19 08:01:22 +0100

    doesn't display

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace WpfApp1
         {
             InitializeComponent();
             cislo = 0;
-            displayValue = "0";
+            displayValue = "";
 
             Cislo_0.Click += (sender, e) => AppendNumber(0, "0");
             Cislo_1.Click += (sender, e) => AppendNumber(1, "1");
@@ -37,17 +37,35 @@ namespace WpfApp1
             Cislo_8.Click += (sender, e) => AppendNumber(8, "8");
             Cislo_9.Click += (sender, e) => AppendNumber(9, "9");
 
+            ZapornyToggle.Click += (sender, e) => NegateValue();
 
+            Label.Content = displayValue;
         }
         private void AppendNumber(int number, string numberString)
         {
             displayValue += numberString;
-            number.
+            cislo = int.Parse(numberString);
         }
         private void NegateValue()
         {
-            currentValue *= -1;
-            Label.Content = currentValue.ToString();
+            // when negative already
+            if (displayValue[0] == '-')
+            {
+                // displayValue is now everythng from 1st index to end (cuts off 1st)
+                displayValue = displayValue[1..];
+                cislo *= -1;
+            }
+            // when positive
+            else
+            {
+                displayValue = "-" + displayValue;
+                cislo *= -1;
+            }
+        }
+
+        private void Label_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
